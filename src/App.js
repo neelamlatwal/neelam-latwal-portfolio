74c53fe3b3b12c08bdd1ce63e02156b178782227
neelamlatwal <nitulatwal@gmail.com>
2024-10-07 16:11:17 +0530

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import ReactGA from 'react-ga4';
+
 import Loader from './components/Loader'
 import NotFound from './components/NotFound'
 import Resume from "./pages/Resume"
@@ -8,6 +10,10 @@ import Resume from "./pages/Resume"
 const Main = lazy(() => import('./pages/Main'));
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-508LVLNMGB'); // Use your tracking ID here
+    ReactGA.send('pageview');
+  }, []);
   return (
     <div className="App">
 
